67fef29beab02c7cb834574fb4dea0e2cfd9b120
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-06-01 11:46:22 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDyum8X-YyHftZU2zksQpTq2oI-BhWwzuM",
+    authDomain: "tic-tac-toe-572af.firebaseapp.com",
+    databaseURL: "https://tic-tac-toe-572af.firebaseio.com",
+    projectId: "tic-tac-toe-572af",
+    storageBucket: "tic-tac-toe-572af.appspot.com",
+    messagingSenderId: "719563127796",
+    appId: "1:719563127796:web:b72edc0bff0c0330e917a1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
